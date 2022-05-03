bba0e534907259f7d10d556f69e3323d53b1a4dd
Yan Cang <alailaikeai@163.com>
2022-05-04 00:42:22 +0800

    0503 到此为止

--- mall-product/src/main/java/com/hzy/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/hzy/mall/product/MallProductApplication.java
@@ -1,8 +1,17 @@
 package com.hzy.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1. 导入依赖
+ * 2. 导入数据库驱动
+ * 3. 配置yml文件
+ * 4. @MapperScan
+ * 5. 告诉MP sql映射文件的位置
+ */
+@MapperScan("com.hzy.mall.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
 
--- mall-product/src/test/java/com/hzy/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/hzy/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,23 @@
 package com.hzy.mall.product;
 
+import com.hzy.mall.product.entity.BrandEntity;
+import com.hzy.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+    @Autowired
+    private BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setDescript("acer");
+        brandService.save(brandEntity);
+        System.out.println("保存成功！");
     }
 
 }
